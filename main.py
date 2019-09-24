55619183449178e2abb7958489b51a64a5eacaa0
Derek <derek@Delight.local>
2019-09-24 14:12:39 -0700

    Append more scraped fields to csv

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 import requests
 import urllib.request
 
-# Setup [ ]
+# Setup [  ]
 
 base_url = 'https://www.premierleague.com/match/'
 
@@ -57,23 +57,21 @@ f.write(label_str)
 # To do [ ]
 
 # For each match, do the following:
-	# 1) Get the 3-letter abbreviation for the home team, append to file, append a comma	[OK]
-	# 2) Get the % possession value for the home team, append to file, append a comma
-	# 3) Get the number of goals scored by the home team, append to file, append a comma
-	# 4) Get the number of points earned by the home team, append to file, append a comma
-	# 5) Get the 3-letter abbrevation for the away team, append to file, append a comma		[OK]
-	# 6) Get the % possession value for the away team, append to file, append a comma
-	# 7) Get the number of goals scored by the away team, append to file, append a comma
-	# 8) Get the number of points earned by the away team, append to file, append a newline
+	# 1) Get the 3-letter abbreviation for the home team	[OK]
+	# 2) Get the % possession value for the home team
+	# 3) Get the number of goals scored by the home team	[OK]
+	# 4) Get the 3-letter abbrevation for the away team		[OK]
+	# 5) Get the % possession value for the away team
+	# 6) Get the number of goals scored by the away team	[OK]
+	# 7) Get the number of points earned by the home team	[OK]
+	# 8) Get the number of points earned by the away team	[OK]
+	# 9) Append 1,2,3,7,4,5,6,8,newline						[  ]
 
 
-# 1) Get the 3-letter abbreviation for the home team, append to file, append a comma	[OK]
-
+# 1) Get the 3-letter abbreviation for the home team	[OK]
 name_home = soup.find('div', class_='teamScore').find_all('a')[0].contents[2][5:8];
-f.write(name_home)
-f.write(",")
 
-# 2) Get the % possession value for the home team, append to file, append a comma
+# 2) Get the % possession value for the home team		[  ]
 
 # Use selenium webdriver to get data from dynamically generated HTML table
 # Dynamically generated table contains % possession data
@@ -107,47 +105,55 @@ f.write(pp_home)
 f.write(",")
 """
 
-# 3) Get the number of goals scored by the home team, append to file, append a comma	[OK]
-
+# 3) Get the number of goals scored by the home team	[OK]
 gs_home = soup.find('div', class_='score').contents[0]
+
+# 4) Get the 3-letter abbrevation for the away team		[OK]
+name_away = soup.find('div', class_='teamScore').find_all('a')[1].contents[0][5:8];
+
+# 5) Get the % possession value for the away team		[  ]
+
+
+
+# 6) Get the number of goals scored by the away team	[OK]
+gs_away = soup.find('div', class_='score').contents[2]
+
+# 7) Get the number of points earned by the home team	[OK]
+pe_home = 0
+if (gs_home > gs_away):
+	pe_home = 3
+elif (gs_home == gs_away):
+	pe_home = 1
+else:
+	pe_home = 0
+
+# 8) Get the number of points earned by the away team	[OK]
+pe_away = 0
+if (gs_away > gs_home):
+	pe_away = 3
+elif (gs_away == gs_home):
+	pe_away = 1
+else:
+	pe_away = 0
+
+# 9) Append 1,2,3,7,4,5,6,8,newline						[ ]
+
+f.write(name_home)
+f.write(",")
+
 f.write(gs_home)
 f.write(",")
 
-# 4) Get the number of points earned by the home team, append to file, append a comma
+f.write(str(pe_home))
+f.write(",")
 
-"""
-def	calculate_points_earned(gsh, gsa):
-	gs_home, gs_away = gsh, gsa
-	pe_home, pe_away = 0, 0
-
-	if (gs_home > gs_away):
-		pe_home = 3
-		pe_away = 0
-	elif (gs_home < gs_away):
-		pe_home = 0
-		pe_away = 3
-	else:
-		pe_home, pe_away = 1
-
-calculate_points_earned(gs_home, gs_away)
-"""
-
-
-# 5) Get the 3-letter abbrevation for the away team, append to file, append a comma		[OK]
-
-name_away = soup.find('div', class_='teamScore').find_all('a')[1].contents[0][5:8];
 f.write(name_away)
 f.write(",")
 
-# 6) Get the % possession value for the away team, append to file, append a comma
-
-
-# 7) Get the number of goals scored by the away team, append to file, append a comma
-
-gs_away = soup.find('div', class_='score').contents[2]
 f.write(gs_away)
 f.write(",")
 
-# 8) Get the number of points earned by the away team, append to file, append a newline
+f.write(str(pe_away))
+f.write("\n")
 
 f.close()
