db79d891d077c13a855205fa891a6fe791e1eeb8
Derek <derek@Delight.local>
2019-10-22 03:07:07 -0700

    Display gross avg_pp and gross avg_pe scatterplot

--- generate_visuals.py
+++ generate_visuals.py
@@ -7,8 +7,6 @@ import numpy as np
 import pandas as pd
 import seaborn as seabornInstance
 
-# %matplotlib inline
-
 path = 'epl_data.csv'
 dataset = pd.read_csv(path)
 
@@ -54,76 +52,26 @@ teams = {
 	'WOL': dataset[(dataset.name_home == 'WOL') | (dataset.name_away == 'WOL')]
 }
 
-# print(dataset)
-
+pp_pe = {}
 
 for team, dataset in teams.items():
 	name_home = dataset[(dataset.name_home == team)]
 	name_away = dataset[(dataset.name_away == team)]
+	
 	home_avg_pp = sum(name_home.pp_home) / len(name_home.pp_home)
 	away_avg_pp = sum(name_away.pp_away) / len(name_away.pp_away)
 	avg_pp = (home_avg_pp + away_avg_pp) / 2
 
-"""
-ars = dataset[(dataset.name_home == 'ARS') | (dataset.name_away == 'ARS')]
-ars_home = ars.loc[ars.name_home == 'ARS']
-ars_away = ars.loc[ars.name_away == 'ARS']
+	home_avg_pe = sum(name_home.pe_home) / len(name_home.pe_home)
+	away_avg_pe = sum(name_away.pe_away) / len(name_away.pe_away)
+	avg_pe = (home_avg_pe + away_avg_pe) / 2
 
-ars_home_avg_pp = sum(ars_home.pp_home) / len(ars_home.pp_home)
-ars_away_avg_pp = sum(ars_away.pp_away) / len(ars_away.pp_away)
-ars_avg_pp = (ars_home_avg_pp + ars_away_avg_pp) / 2
+	pp_pe[avg_pp] = avg_pe
 
-ars_home_avg_pe = sum(ars_home.pe_home) / len(ars_home.pe_home)
-ars_away_avg_pe = sum(ars_away.pe_away) / len(ars_away.pe_away)
-ars_avg_pe = (ars_home_avg_pe + ars_away_avg_pe) / 2
+full = pd.DataFrame(list(pp_pe.items()), columns=['avg_pp', 'avg_pe'])
 
-print('ARS average possession at home:', ars_home_avg_pp)
-print('ARS average possession away:', ars_away_avg_pp)
-print('ARS average possession:', ars_avg_pp)
-
-print('ARS average points earned at home:', ars_home_avg_pe)
-print('ARS average points earned away:', ars_away_avg_pe)
-print('ARS average points earned:', ars_avg_pe)
-"""
-
-"""
-ars = dataset[(dataset.name_home == 'ARS') | (dataset.name_away == 'ARS')]
-avl = dataset[(dataset.name_home == 'AVL') | (dataset.name_away == 'AVL')]
-bha = dataset[(dataset.name_home == 'BHA') | (dataset.name_away == 'BHA')]
-bir = dataset[(dataset.name_home == 'BIR') | (dataset.name_away == 'BIR')]
-blb = dataset[(dataset.name_home == 'BLB') | (dataset.name_away == 'BLB')]
-blp = dataset[(dataset.name_home == 'BLP') | (dataset.name_away == 'BLP')]
-bol = dataset[(dataset.name_home == 'BOL') | (dataset.name_away == 'BOL')]
-bou = dataset[(dataset.name_home == 'BOU') | (dataset.name_away == 'BOU')]
-bur = dataset[(dataset.name_home == 'BUR') | (dataset.name_away == 'BUR')]
-car = dataset[(dataset.name_home == 'CAR') | (dataset.name_away == 'CAR')]
-cha = dataset[(dataset.name_home == 'CHA') | (dataset.name_away == 'CHA')]
-che = dataset[(dataset.name_home == 'CHE') | (dataset.name_away == 'CHE')]
-cry = dataset[(dataset.name_home == 'CRY') | (dataset.name_away == 'CRY')]
-der = dataset[(dataset.name_home == 'DER') | (dataset.name_away == 'DER')]
-eve = dataset[(dataset.name_home == 'EVE') | (dataset.name_away == 'EVE')]
-ful = dataset[(dataset.name_home == 'FUL') | (dataset.name_away == 'FUL')]
-hud = dataset[(dataset.name_home == 'HUD') | (dataset.name_away == 'HUD')]
-hul = dataset[(dataset.name_home == 'HUL') | (dataset.name_away == 'HUL')]
-lei = dataset[(dataset.name_home == 'LEI') | (dataset.name_away == 'LEI')]
-liv = dataset[(dataset.name_home == 'LIV') | (dataset.name_away == 'LIV')]
-mci = dataset[(dataset.name_home == 'MCI') | (dataset.name_away == 'MCI')]
-mid = dataset[(dataset.name_home == 'MID') | (dataset.name_away == 'MID')]
-mun = dataset[(dataset.name_home == 'MUN') | (dataset.name_away == 'MUN')]
-new = dataset[(dataset.name_home == 'NEW') | (dataset.name_away == 'NEW')]
-nor = dataset[(dataset.name_home == 'NOR') | (dataset.name_away == 'NOR')]
-por = dataset[(dataset.name_home == 'POR') | (dataset.name_away == 'POR')]
-qpr = dataset[(dataset.name_home == 'QPR') | (dataset.name_away == 'QPR')]
-rdg = dataset[(dataset.name_home == 'RDG') | (dataset.name_away == 'RDG')]
-shu = dataset[(dataset.name_home == 'SHU') | (dataset.name_away == 'SHU')]
-sou = dataset[(dataset.name_home == 'SOU') | (dataset.name_away == 'SOU')]
-stk = dataset[(dataset.name_home == 'STK') | (dataset.name_away == 'STK')]
-sun = dataset[(dataset.name_home == 'SUN') | (dataset.name_away == 'SUN')]
-swa = dataset[(dataset.name_home == 'SWA') | (dataset.name_away == 'SWA')]
-tot = dataset[(dataset.name_home == 'TOT') | (dataset.name_away == 'TOT')]
-wat = dataset[(dataset.name_home == 'WAT') | (dataset.name_away == 'WAT')]
-wba = dataset[(dataset.name_home == 'WBA') | (dataset.name_away == 'WBA')]
-whu = dataset[(dataset.name_home == 'WHU') | (dataset.name_away == 'WHU')]
-wig = dataset[(dataset.name_home == 'WIG') | (dataset.name_away == 'WIG')]
-wol = dataset[(dataset.name_home == 'WOL') | (dataset.name_away == 'WOL')]
-"""
+full.plot(x='avg_pp', y='avg_pe', style='o')
+plt.title('Average possession versus match outcomes')
+plt.xlabel('Average possession percentage per match (avg_pp)')
+plt.ylabel('Average points earned per match (avg_pe)')
+plt.show()
